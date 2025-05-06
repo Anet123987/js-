8aed947a8194b1349133c67d9f1bbd0282d50be2
Anna Taranova <annsheremet123@gmail.com>
2025-05-06 13:56:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -78,22 +78,127 @@
 // Якщо число ділитися  без остачі на 3 - виводить в консоль '<число> - fizz',
 // якщо ділиться  без остачі на 5 - виводить в консоль '<число> - buzz',
 // якщо ділиться  без остачі і на 3, і на 5 - виводить в консоль '<число> - fizzbuzz'.
-function fizzBuzz(num) {
-    for (let i = 1; i < num; i += 1) {
-        if ((i % 3 === 0) && (i % 5 === 0)) {
-            console.log(`${i}- fizzbuzz`);
-        }
+// function fizzBuzz(num) {
+//     for (let i = 1; i < num; i += 1) {
+//         if ((i % 3 === 0) && (i % 5 === 0)) {
+//             console.log(`${i}- fizzbuzz`);
+//         }
 
-        else if (i % 3 === 0) {
-            console.log(`${i}- fizz`);
-        }
-        else if (i % 5 === 0) {
-            console.log(`${i}- buzz`);
-        }
+//         else if (i % 3 === 0) {
+//             console.log(`${i}- fizz`);
+//         }
+//         else if (i % 5 === 0) {
+//             console.log(`${i}- buzz`);
+//         }
 
-        else {
-            console.log(`${i}`);
-        }
+//         else {
+//             console.log(`${i}`);
+//         }
+//     }
+// }
+// fizzBuzz(200);   
+// // Create promise
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (isSuccess) {
+//       resolve("Success! Value passed to resolve function");
+//     } else {
+//       reject("Error! Error passed to reject function");
+//     }
+//   }, 2000);
+// });
+
+// // Registering promise callbacks
+// promise
+//   .then(value => console.log(value)) // "Success! Value passed to resolve function"
+//   .catch(error => console.log(error)) // "Error! Error passed to reject function"
+//   .finally(() => console.log("Promise settled")); // "Promise settled"
+//   const fetchUserFromServer = (username, onSuccess, onError) => {
+//     console.log(`Fetching data for ${username}`);
+// const fetchUserFromServer = (username) => {
+//     return new Promise((resolve, reject) => {
+// 		// ...
+//   });};
+//   const userPromise = fetchUserFromServer("Mango"); // 
+    
+  
+//   fetchUserFromServer(
+//       "Mango", 
+//       user => console.log(user), 
+//       error => console.error(error)
+//   );
+  
+  
+//   const makePromise = ({ value, delay, shouldResolve = true }) => {
+//     return new Promise((resolve, reject) => {
+//          setTimeout(() => {
+//                   if(shouldResolve) {
+//                       resolve(value)
+//                   } else {
+//                       reject(value)
+//                   }
+//               }, delay);
+//     });
+//   };
+  
+//   makePromise({ value: "A", delay: 1000 })
+//       .then(value => console.log(value)) // "A"
+//       .catch(error => console.log(error));
+  
+//   makePromise({ value: "B", delay: 100 })
+//       .then(value => console.log(value)) // "B"
+//       .catch(error => console.log(error));
+  
+//   makePromise({ value: "C", delay: 2000, shouldResolve: false })
+//       .then(value => console.log(value)) 
+//       .catch(error => console.log(error)); // "C"
+  
+// const p1= new Promise((resolve,reject)=>{
+    //    setTimeout(()=>{
+    //     const random= Math.random();
+    //     if (random>0.5) {
+    //         resolve("can eat!");
+    //     }else{
+    //         reject("come for money");
+    //     }
+    //   });
+    // })
+    //   p1.then(onFull=>{
+    //     console.log("nyam")
+    //   })
+    //   .catch(onRej=>{
+    //     console.log("money")
+    //   })
+    // const promise= new Promise((resolve,reject)=>{
+    //     resolve(10);})
+    // promise.then(()=>{})
+    // .then(()=>{})
+    // .then(()=>{})
+    // .catch(()=>{})
+    
+// Promise.reject()
+// Promise.resolve()
+// // Promise.all-чекає всі,якщо не має помилок.результат масив всіх промісів,або першу помилку
+// Promise.allSettled повертає масив зі статусом виконання,і виконані і не виконані
+// Promise.rase повертає перший проміс,незалежно від виконання
+const value = +prompt("Give a value");
+
+function checkValue(value) {
+  return new Promise((res, rej) => {
+    if (Number.isNaN(value)) {
+      rej("Error: Not a number");
+    } else if (value % 2 === 0) {
+      setTimeout(() => {
+        res("even");
+      }, 1000);
+    } else {
+      setTimeout(() => {
+        res("odd");
+      }, 2000);
     }
+  });
 }
-fizzBuzz(200);   
+
+checkValue(value)
+  .then(result => console.log("Result:", result))
+  .catch(error => console.error("Error:", error))
